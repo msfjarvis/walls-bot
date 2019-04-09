c46fa2490ca9310b9f539bd6a18ffb15c27e889c
Harsh Shandilya <msfjarvis@gmail.com>
2019-04-09 23:28:12 +0530

    Add support for /random
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/main/kotlin/me/msfjarvis/wallsbot/Main.kt
+++ src/main/kotlin/me/msfjarvis/wallsbot/Main.kt
@@ -4,10 +4,12 @@ import me.ivmg.telegram.bot
 import me.ivmg.telegram.dispatch
 import me.ivmg.telegram.dispatcher.command
 import me.ivmg.telegram.entities.ParseMode
+import me.ivmg.telegram.network.fold
 import okhttp3.logging.HttpLoggingInterceptor
 import java.io.File
 import java.io.FileInputStream
 import java.util.*
+import kotlin.random.Random
 
 fun main() {
     val props = Properties()
@@ -22,7 +24,7 @@ fun main() {
     val bot = bot {
         token = props.getProperty("botToken")
         timeout = 30
-        logLevel = HttpLoggingInterceptor.Level.BODY
+        logLevel = HttpLoggingInterceptor.Level.BASIC
         dispatch {
             command("search") { bot, update, args ->
                 val foundFiles = HashSet<String>()
@@ -39,6 +41,31 @@ fun main() {
                             parseMode = ParseMode.MARKDOWN,
                             disableWebPagePreview = true
                     )
+                }
+            }
+            command("random") { bot, update ->
+                val allFiles = File(searchDir).listFiles()
+                val randomInt = Random.nextInt(0, allFiles.size)
+                val fileToSend = allFiles[randomInt]
+                update.message?.let { message ->
+                    try {
+                        val msg = bot.sendPhoto(
+                                chatId = message.chat.id,
+                                photo = fileToSend,
+                                caption = "[${fileToSend.name}]($baseUrl/${fileToSend.name})",
+                                replyToMessageId = message.messageId
+                        )
+                        msg.fold({ },{
+                            println(it.exception.toString())
+                        })
+                    } catch (ignored: Exception) {
+                        bot.sendDocument(
+                                chatId = message.chat.id,
+                                document = fileToSend,
+                                caption = "[${fileToSend.name}]($baseUrl/${fileToSend.name})",
+                                replyToMessageId = message.messageId
+                        )
+                    }
                 }
             }
         }
