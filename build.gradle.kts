10b370f1320a14ed6def04cce455a6538e7b839c
Harsh Shandilya <msfjarvis@gmail.com>
2019-05-13 22:39:42 +0530

    Update all deps
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.3.30"
+    kotlin("jvm") version "1.3.31"
     id("org.jlleitschuh.gradle.ktlint") version "4.1.0"
     id("com.github.johnrengelman.shadow") version "5.0.0"
 }
@@ -28,11 +28,18 @@ repositories {
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib")
-    compile("io.github.seik.kotlin-telegram-bot:telegram:0.3.7")
+    compile("io.github.seik.kotlin-telegram-bot:telegram:0.3.8")
 }
 
 val jar by tasks.getting(Jar::class) {
     manifest {
         attributes["Main-Class"] = "me.msfjarvis.wallsbot.MainKt"
+    }
+}
+
+tasks {
+    named<Wrapper>("wrapper") {
+        gradleVersion = "5.4.1"
+        distributionType = Wrapper.DistributionType.ALL
     }
 }
