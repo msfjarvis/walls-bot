08a4dfae46d34eb70fc70db540628734a787e40b
Harsh Shandilya <msfjarvis@gmail.com>
2019-08-07 11:46:54 +0530

    Sort commands in alphabetical order
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/main/kotlin/me/msfjarvis/wallsbot/WallsBot.kt
+++ src/main/kotlin/me/msfjarvis/wallsbot/WallsBot.kt
@@ -42,92 +42,6 @@ class WallsBot : CoroutineScope {
             timeout = 30
             logLevel = if (props.debug) HttpLoggingInterceptor.Level.BASIC else HttpLoggingInterceptor.Level.NONE
             dispatch {
-                command("search") { bot, update, args ->
-                    launch {
-                        if (args.isEmpty()) {
-                            update.message?.let { message ->
-                                bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
-                                bot.sendMessage(
-                                        chatId = message.chat.id,
-                                        text = "No arguments supplied!",
-                                        replyToMessageId = message.messageId
-                                )
-                            }
-                            return@launch
-                        }
-                        val foundFiles = HashSet<String>()
-                        File(props.searchDir).listFiles().forEach { file ->
-                            if (file.nameWithoutExtension.startsWith(args.joinToString())) {
-                                foundFiles.add("[${file.sanitizedName}](${props.baseUrl}/${file.name})")
-                            }
-                        }
-                        update.message?.let { message ->
-                            bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
-                            bot.sendMessage(
-                                    chatId = message.chat.id,
-                                    replyToMessageId = message.messageId,
-                                    text = foundFiles.joinToString("\n"),
-                                    parseMode = ParseMode.MARKDOWN,
-                                    disableWebPagePreview = true
-                            )
-                        }
-                    }
-                }
-
-                if (props.ownerId != null) {
-                    command("stats") { bot, update ->
-                        launch {
-                            update.message?.let { message ->
-                                val messageFrom: Long = message.from?.id ?: 0
-                                if (messageFrom != props.ownerId) {
-                                    bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
-                                    bot.sendMessage(
-                                            chatId = message.chat.id,
-                                            text = "Unauthorized!",
-                                            replyToMessageId = message.messageId
-                                    )
-                                    return@launch
-                                }
-                            }
-                            val allFiles = File(props.searchDir).listFiles()
-                            var diskSpace: Long = 0
-                            for (file in allFiles) {
-                                diskSpace += file.length()
-                            }
-                            val units = arrayOf("B", "KB", "MB", "GB", "TB")
-                            val digitGroups: Double = floor((log10(diskSpace.toDouble()) / log10(1024.0)))
-                            val decimalFormat = DecimalFormat("#,##0.##")
-                                    .format(diskSpace / 1024.0.pow(digitGroups)) + " " + units[digitGroups.toInt()]
-                            update.message?.let { message ->
-                                bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
-                                bot.sendMessage(
-                                        chatId = message.chat.id,
-                                        text = "Total files : ${allFiles.size} \nDisk space used : $decimalFormat",
-                                        replyToMessageId = message.messageId
-                                )
-                            }
-                        }
-                    }
-                }
-
-                command("random") { bot, update ->
-                    launch {
-                        val allFiles = File(props.searchDir).listFiles()
-                        val randomInt = Random.nextInt(0, allFiles.size)
-                        val fileToSend = allFiles[randomInt]
-                        update.message?.let { message ->
-                            bot.sendPictureSafe(
-                                    repository,
-                                    message.chat.id,
-                                    props.baseUrl,
-                                    fileToSend,
-                                    message.messageId,
-                                    genericCaption = props.genericCaption
-                            )
-                        }
-                    }
-                }
-
                 command("pic") { bot, update, args ->
                     launch {
                         if (args.isEmpty()) {
@@ -195,6 +109,92 @@ class WallsBot : CoroutineScope {
                             db.commit()
                             db.close()
                             exitProcess(0)
+                        }
+                    }
+                }
+
+                command("random") { bot, update ->
+                    launch {
+                        val allFiles = File(props.searchDir).listFiles()
+                        val randomInt = Random.nextInt(0, allFiles.size)
+                        val fileToSend = allFiles[randomInt]
+                        update.message?.let { message ->
+                            bot.sendPictureSafe(
+                                    repository,
+                                    message.chat.id,
+                                    props.baseUrl,
+                                    fileToSend,
+                                    message.messageId,
+                                    genericCaption = props.genericCaption
+                            )
+                        }
+                    }
+                }
+
+                command("search") { bot, update, args ->
+                    launch {
+                        if (args.isEmpty()) {
+                            update.message?.let { message ->
+                                bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
+                                bot.sendMessage(
+                                        chatId = message.chat.id,
+                                        text = "No arguments supplied!",
+                                        replyToMessageId = message.messageId
+                                )
+                            }
+                            return@launch
+                        }
+                        val foundFiles = HashSet<String>()
+                        File(props.searchDir).listFiles().forEach { file ->
+                            if (file.nameWithoutExtension.startsWith(args.joinToString())) {
+                                foundFiles.add("[${file.sanitizedName}](${props.baseUrl}/${file.name})")
+                            }
+                        }
+                        update.message?.let { message ->
+                            bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
+                            bot.sendMessage(
+                                    chatId = message.chat.id,
+                                    replyToMessageId = message.messageId,
+                                    text = foundFiles.joinToString("\n"),
+                                    parseMode = ParseMode.MARKDOWN,
+                                    disableWebPagePreview = true
+                            )
+                        }
+                    }
+                }
+
+                if (props.ownerId != null) {
+                    command("stats") { bot, update ->
+                        launch {
+                            update.message?.let { message ->
+                                val messageFrom: Long = message.from?.id ?: 0
+                                if (messageFrom != props.ownerId) {
+                                    bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
+                                    bot.sendMessage(
+                                            chatId = message.chat.id,
+                                            text = "Unauthorized!",
+                                            replyToMessageId = message.messageId
+                                    )
+                                    return@launch
+                                }
+                            }
+                            val allFiles = File(props.searchDir).listFiles()
+                            var diskSpace: Long = 0
+                            for (file in allFiles) {
+                                diskSpace += file.length()
+                            }
+                            val units = arrayOf("B", "KB", "MB", "GB", "TB")
+                            val digitGroups: Double = floor((log10(diskSpace.toDouble()) / log10(1024.0)))
+                            val decimalFormat = DecimalFormat("#,##0.##")
+                                    .format(diskSpace / 1024.0.pow(digitGroups)) + " " + units[digitGroups.toInt()]
+                            update.message?.let { message ->
+                                bot.sendChatAction(chatId = message.chat.id, action = ChatAction.TYPING)
+                                bot.sendMessage(
+                                        chatId = message.chat.id,
+                                        text = "Total files : ${allFiles.size} \nDisk space used : $decimalFormat",
+                                        replyToMessageId = message.messageId
+                                )
+                            }
                         }
                     }
                 }
