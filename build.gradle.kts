cacf711b2b17945a6e2bae06110846e75bb975da
Harsh Shandilya <msfjarvis@gmail.com>
2019-08-10 21:39:21 +0530

    build: Upgrade to my fork of kotlin-telegram-bot
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("io.github.seik.kotlin-telegram-bot:telegram:0.3.8")
+    implementation("com.github.msfjarvis:kotlin-telegram-bot:0.4.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.0.1")
     implementation("org.dizitart:potassium-nitrite:3.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC")
 }
