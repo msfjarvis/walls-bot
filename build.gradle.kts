cb09aae8d21ea2ab1a750828ad79cdb4e9cfe70a
Harsh Shandilya <msfjarvis@gmail.com>
2019-09-28 20:34:02 +0530

    Update dependencies
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.github.msfjarvis:kotlin-telegram-bot:0.4.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.1.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.2.0")
     implementation("org.dizitart:potassium-nitrite:3.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
 }
 
 application {
