15c7ee1af61416f6af21b4669e6412aad770adc2
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-23 13:10:32 +0530

    Add basic unit tests
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ src/test/kotlin/me/msfjarvis/wallsbot/ExtensionTests.kt
@@ -0,0 +1,23 @@
+package me.msfjarvis.wallsbot
+
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
+
+class ExtensionTests {
+    @Test
+    fun `sanitizing file names`() {
+        assertEquals(File("Katherine_McNamara_2.jpg").sanitizedName, "Katherine McNamara 2")
+        assertEquals(File("Emma_Dumont_4.jpg").sanitizedName, "Emma Dumont 4")
+        assertNotEquals(File("Cara_Delevingne_1.jpg").sanitizedName, "Cara_Delevingne_1")
+    }
+
+    @Test
+    fun `calculating MD5`() {
+        val sampleFile = javaClass.classLoader.getResource("sample_file")?.file
+        assertNotNull(sampleFile)
+        assertEquals(File(sampleFile).calculateMD5(), "8d51b2297b2fcd67f1959bc407c94e5c")
+    }
+}
