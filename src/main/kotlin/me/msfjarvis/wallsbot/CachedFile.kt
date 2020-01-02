9265a73c7f133643aa98103d28bdae4ed27320c0
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-03 03:11:16 +0530

    Remove unused CachedFile
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/main/kotlin/me/msfjarvis/wallsbot/CachedFile.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
- * SPDX-License-Identifier: MIT
- */
-package me.msfjarvis.wallsbot
-
-data class CachedFile(val fileId: String, val fileHash: String)
